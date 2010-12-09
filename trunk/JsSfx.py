b510f372d816c25b937bbccd0dcdeb2d45a49b8c
berendjanwever@gmail.com <berendjanwever@gmail.com@c3fcc651-ac40-ae4b-e4e1-c736d0784bf7>
2010-12-09 20:32:34 +0000

    Output beautified/modified to show progress. Updated JsSfx3 to 3.2.

--- trunk/JsSfx.py
+++ trunk/JsSfx.py
@@ -1,72 +1,82 @@
+# -*- coding: latin1 -*-
 # Copyright (c) 2006-2010 Berend-Jan "SkyLined" Wever <berendjanwever@gmail.com>
 # Project homepage: http://code.google.com/p/jssfx/
 # All rights reserved. See COPYRIGHT.txt for details.
 import os, sys;
 
-from JsSfx12 import JsSfx12;
-from JsSfx3 import JsSfx3;
 from Strip import Strip;
+from JsSfx12 import JsSfx12;
+from JsSfx32 import JsSfx32;
+
+ASCII_JS_CHARS =  '\1\2\3\4\5\6\7\b\t\r\x0b\f\n\x0e\x0f' \
+                  '\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f' \
+                  ' !"#$%&\'()*+,-./' \
+                  '0123456789:;<=>?'  \
+                  '@ABCDEFGHIJKLMNO'  \
+                  'PQRSTUVWXYZ[\\]^_' \
+                  '`abcdefghijklmno'  \
+                  'pqrstuvwxyz{|}~\x7f';
+LATIN1_JS_CHARS = ASCII_JS_CHARS +    \
+                  ''                  \
+                  ''                  \
+                  ' ¡¢£¤¥¦§¨©ª«¬­®¯'  \
+                  '°±²³´µ¶·¸¹º»¼½¾¿'  \
+                  'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ'  \
+                  'ÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞß'  \
+                  'àáâãäåæçèéêëìíîï'  \
+                  'ðñòóôõö÷øùúûüýþÿ';
 
 def PrintUsage():
   print """JsSfx - Tool for creating self-extracting compressed JavaScript.
   Copyright (c) 2006, 2010 Berend-jan "SkyLined" Wever.
-  Compression algorithm replaces strings of bytes that are repeated in the
-  data with shorter sequences of bytes that do not appear in the data, eg.
-  'Abcdef.Abcdef.Abcdef.' could be encoded as ('X', 'XXXAbcdef.'). Output is
-  a piece of JavaScript that contains the compressed data and will decompressed
-  it when it is executed. Once the data is decompressed, it is executed using
-  "eval()".
+  JsSfx removes comments and superfluous separation characters (whitespace and
+  semi-colons) from an input JavaScript and applies various compression
+  techniques with various settings to it. It outputs the smallest self-
+  extracting compressed version of the script that it created.
 
 Usage:
   JsSfx.py input_file output_file [options]
 
 Options:
-  --strip
-     Remove comments and superfluous whitespace and semi-colons from input
+  --no-strip
+     Do not remove comments and superfluous separation characters from the input
      JavaScript before compression.
-  --strip-only
-     Same as --strip, but do not compress the data.
+  --no-compress
+     Do not try to compress the script.
+  --ascii
+     Use only ASCII characters (00-7F) rather than latin-1 (00-7F+A0-FF).
   --log-level=number
      Specify the amount of output to show during compression. 0 = limited, 1 =
      verbose, 2 = very verbose.
-  --max-unused-str-len=number
-     Force using a specific decoder stub.
-     Use "1" for a small decoder stub with compression limited to replacing
-     sequences of bytes with unused single bytes. This works best for small
-     files that do not use many different characters.
-     Use "2" for s slightly larger decoder which allows for more compression.
-     This works better with larger files or files that use many different bytes
-     because more bytes could be saved by the additional compression than are
-     added because of the larger decoder stub, making the output smaller.
-     Currently, only "1" and "2" are supported, as larger unused string sizes
-     are not expected to be required to encode any JavaScript file of sane size.
-     by default, the program will try "1" first and if additional compression
-     is possible, it will try "2" as well and choose whichever yielded the
-     smallest result.""";
+  --exhaustive
+     By default, not all possible ways to use all available characters during
+     compression are applied (to speed up the compression process). Use this
+     option to have the code try them all and find the best one. This may
+     slightly increase compression in some cases, but requires a lot more time
+     to process the script.
+""";
 
 def Main(*argv):
   input_file_path = None;
   output_file_path = None;
-  max_unused_str_len = None;
   log_level = 0;
-  use_strip = False;
-  strip_only = False;
+  use_ascii = False;
+  use_strip = True;
+  use_compress = True;
+  quick_and_dirty = True;
   for arg in argv:
     if arg.startswith('-'):
       if arg in ['-?', '-h', '--help']:
         PrintUsage();
         return True;
-      elif arg == '--strip':
-        use_strip = True;
-      elif arg == '--strip-only':
-        use_strip = True;
-        strip_only = True;
-      elif arg.startswith('--max-unused-str-len='):
-        max_unused_str_len = int(arg[len('--max-unused-str-len='):]);
-        if max_unused_str_len not in [1, 2]:
-          print 'max-unused-str-len value must be 1 or 2.';
-          PrintUsage();
-          return False;
+      elif arg == '--no-strip':
+        use_strip = False;
+      elif arg == '--no-compress':
+        use_compress = False;
+      elif arg == '--ascii':
+        use_ascii = True;
+      elif arg == '--exhaustive':
+        quick_and_dirty = False;
       elif arg.startswith('--log-level='):
         log_level = int(arg[len('--log-level='):]);
         if log_level not in [0, 1, 2]:
@@ -107,11 +117,46 @@ def Main(*argv):
 
     input_file_content = input_file_handle.read();
     data = input_file_content;
+    print;
+    print ''.ljust(80, '=');
+    print ' JsSfx - JavaScript self-extracting compressed script generator.';
+    print ' Copyright (c) 2006-2010 Berend-Jan "SkyLined" Wever <berendjanwever@gmail.com>';
+    print ' Project homepage: http://code.google.com/p/jssfx/';
+    print ''.ljust(80, '=');
+    print;
+    print ' Size   Remarks';
+    print '======v'.ljust(80, '=');
+    print '%5d | Original size' % len(data);
     if use_strip:
       data = Strip(data, log_level);
-    if not strip_only:
-      data = TryJsSfx(data, max_unused_str_len, log_level);
+    if use_compress:
+      if use_ascii:
+        valid_chars = ASCII_JS_CHARS;
+        valid_chars_description = 'ASCII';
+      else:
+        valid_chars = LATIN1_JS_CHARS;
+        valid_chars_description = 'latin1';
+      compressed = [];
+      # Try v1.1
+      js_sfx = JsSfx12(data, valid_chars, valid_chars_description, 1, log_level);
+      js_sfx.Compress();
+      compressed.append(str(js_sfx));
+      if js_sfx.ran_out_of_unused_strs:
+        # Try v1.2
+        js_sfx = JsSfx12(data, valid_chars, valid_chars_description, 2, log_level);
+        js_sfx.Compress();
+        compressed.append(str(js_sfx));
+      # Try v3.1/3.2
+      compressed.append(JsSfx32(data, valid_chars, valid_chars_description, log_level, quick_and_dirty));
+      valid_non_slash_chars = valid_chars;
+      data = compressed[0];
+      # Select whatever yielded the shortest result.
+      for i in range(1, len(compressed)):
+        if len(compressed[i]) < data:
+          data = compressed[i];
     output_file_content = data;
+    print '======^'.ljust(80, '=');
+    print;
     if len(output_file_content) > len(input_file_content):
       print 'The file cannot be compressed.';
       print 'The compressed file is %d bytes larger than the original.' % \
@@ -138,41 +183,6 @@ def Main(*argv):
       input_file_handle.close();
     if output_file_handle is not None:
       output_file_handle.close();
-
-def TryJsSfx(data, max_unused_str_len, log_level):
-  js_sfxs = [];
-  # If max_unused_str_len is 1 or auto, get a JsSfx object for max unused
-  # string size 1:
-  try1 = max_unused_str_len in [None, 1];
-  try2 = max_unused_str_len == 2;
-  print 'Trying JsSfx3...';
-  js_sfxs.append(JsSfx3(data, log_level));
-  if try1:
-    print 'Adding 1 byte compressor and compressing...';
-    js_sfx = JsSfx12(data, 1, log_level);
-    js_sfx.Compress();
-    js_sfxs.append(js_sfx);
-    if js_sfx.ran_out_of_unused_strs:
-      try2 = max_unused_str_len is None;
-      print 'Further compression might be possible.';
-      if not try2:
-        print '(Consider using "--max-unused-str-len=2").';
-  if try2:
-    print 'Adding 2 byte compressor and compressing...';
-    js_sfx = JsSfx12(data, 2, log_level);
-    js_sfx.Compress();
-    js_sfxs.append(js_sfx);
-  # If we only created one JsSfx object, or the first is smaller than the
-  # second, use the first JsSfx object. Otherwise use the second:
-  if len(js_sfxs) == 1 or len(js_sfxs[0]) < len(js_sfxs[1]):
-    if len(js_sfxs) != 1:
-      print 'Using 1 byte unused strings is most efficient.';
-    js_sfx = js_sfxs[0];
-  else:
-    if len(js_sfxs) != 1:
-      print 'Using 2 byte unused strings is most efficient.';
-    js_sfx = js_sfxs[1];
-  return str(js_sfx);
 
 if __name__ == "__main__":
   success = Main(*sys.argv[1:]);
